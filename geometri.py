0f7e127c65767740b3b22a7a60f0a7f6a4cc9db8
jirbthagoras <145314061+jirbthagoras@users.noreply.github.com>
2023-09-18 12:54:43 +0700

    Add files via upload

--- geometri.py
+++ geometri.py
@@ -1,7 +1,6 @@
 
 def persegi():
     num = float(input("masukkan sisi pertama = \n"))
-
     total = num ** 2
     print("{} X {} = {}".format(num, num, total))
 
@@ -18,13 +17,36 @@ def segitiga():
     print("1/2 X {} X {} = {}".format(tinggi, alas, total))
 
 def lingkaran():
-    jarijari = float(input("Masukkan jari jari segitga "))
+    jarijari = float(input("Masukkan jari jari lingkaran "))
     total = 3.14 * jarijari ** 2
     print("PI X {} X {} = {}".format(jarijari, jarijari, total))
 
-while True:
+def pythagoras():
+    select2 = input("Mau hitung sisi A, B, atau C?")
+    if (select2 == "A"):
+        B = int(input("Masukkan panjang sisi B = "))
+        C = int(input("Masukkan panjang sisi C = "))
+        total = (C ** 2 - B ** 2)
+        total **= 1/2
+        print("({} ** 2 - {} ** 2) ** 1/2 = {}".format(B, C, total))
+    elif (select2 == "B"):
+        A = int(input("Masukkan panjang sisi A = "))
+        C = int(input("Masukkan panjang sisi C = "))
+        total = (C ** 2 - B ** 2)
+        total **= 1/2
+        print("({} ** 2 - {} ** 2) ** 1/2 = {}".format(A, C, total))
+    elif (select2 == "C"):
+        A = int(input("Masukkan panjang sisi A = "))
+        B = int(input("Masukkan panjang sisi B = "))
+        total = (A ** 2 + B ** 2)
+        total **= 1/2
+        print("({} ** 2 + {} ** 2) ** 1/2 = {}".format(A, B, total))
+    else:
+        print("Input valid option!")
 
-    select = input("pilih jenis bengun datar yang ingin dihitung luasnya = \n A. Persegi \n B. Persegi panjang \n C. Segitiga \n D. Lingkaran \n")
+
+while True:
+    select = input("pilih jenis bengun datar yang ingin dihitung luasnya = \n A. Persegi \n B. Persegi panjang \n C. Segitiga \n D. Lingkaran \n E. Pythagoras \n")
     if select == "A":
         persegi()
     elif select == "B":
@@ -33,11 +55,15 @@ while True:
         segitiga()
     elif select == "D":
         lingkaran()
-        
+    elif select == "E":
+        pythagoras()
+
 
     maulagigak = input("Mau hitung lagi? Yes/No = \n")
 
     if maulagigak == "Yes":
         pass
     elif maulagigak == "No":
+        break
+    else:
         break
